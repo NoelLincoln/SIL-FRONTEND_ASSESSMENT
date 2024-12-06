edfa3da5ab1d9569300a467c61d82dd9805489e5
Noel Bryant <noelsobryant@gmail.com>
2024-12-06 16:40:08 +0300

    Fix: Move authwrapper inside router, dispatch action to setAuthState.

--- client/src/components/AuthWrapper.tsx
+++ client/src/components/AuthWrapper.tsx
@@ -1,52 +1,29 @@
+
 import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { fetchAuthUser } from "../redux/slices/authSlice";
-import { fetchAlbums } from "../redux/slices/albumSlice";
-import { fetchUsers } from "../redux/slices/userSlice";
-import { AppDispatch } from "../redux/store";
+import { setAuthState } from "../redux/slices/authSlice";
 
 interface AuthWrapperProps {
   children: React.ReactNode;
 }
 
 const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
-    // Log that the fetching process is starting
-    console.log("Dispatching actions to fetch authentication and data...");
+    // Check if query params contain user info
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("id");
+    const email = searchParams.get("email");
+    const name = searchParams.get("name");
 
-    
-
-    // Dispatch actions for authentication and data fetching
-    dispatch(fetchUsers())
-      .unwrap()
-      .then(() => {
-        console.log("Users fetched successfully");
-      })
-      .catch((error) => {
-        console.error("Error fetching users:", error);
-      });
-
-    dispatch(fetchAlbums())
-      .unwrap()
-      .then(() => {
-        console.log("Albums fetched successfully");
-      })
-      .catch((error) => {
-        console.error("Error fetching albums:", error);
-      });
-
-      dispatch(fetchAuthUser())
-    .unwrap()
-      .then(() => {
-        console.log("Auth user found succesfully");
-      })
-      .catch((error) => {
-        console.error("Error fetching auth users:", error);
-      });
-
-  }, [dispatch]);
+    if (id && email && name) {
+      // Dispatch setAuthState if user data is found in query params
+      dispatch(setAuthState({ id, email, name }));
+    }
+  }, [dispatch, location.search]);
 
   return <>{children}</>;
 };
