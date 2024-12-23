2097fb8ac83de37e4621ccd51fb4821ec4d6fa31
Noel Bryant <noelsobryant@gmail.com>
2024-12-23 23:12:18 +0300

    Fix: Remove duplicate users based on unique username in the user list

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
     error: albumsError,
   } = useSelector((state: RootState) => state.albums);
 
-  console.log("Users",users)
+  console.log("Users", users);
 
   // Calculate the number of albums for each user
   const getUserAlbumCount = useCallback(
@@ -35,13 +35,18 @@ const Home: React.FC = () => {
     return <div>Error: {usersError || albumsError}</div>;
   }
 
+  // Filter out duplicate users based on the unique username
+  const uniqueUsers = Array.from(
+    new Map(users.map((user) => [user.username, user])).values()
+  );
+
   return (
     <>
       <div className="bg-gray-100 min-h-screen p-6">
         <div className="container mx-auto">
           <h1 className="text-3xl font-bold text-center mb-8">Users List</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {users.map((user) => (
+            {uniqueUsers.map((user) => (
               <UserCard
                 key={user.id}
                 user={user}
